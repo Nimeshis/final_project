d29be6ad143e08166c0898a217391745015158de
Nimesh <dhungelnimesh5@gmail.com>
2024-02-28 20:12:47 +0545

    mnaging slider in home page

--- src/costumer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/costumer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const HomeSectionCard = () => {
   return (
-    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3'>
-       <div className='h-[13rem] w-[10rem]'>
+    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-xlg overflow-hidden w-[15rem] mx-3 '>
+       <div className='h-[13rem] w-[10rem] border border-black'>
         <img className="object-cover object-top w-full h-full" src='http://surl.li/raebu' alt=''/> 
         </div> 
-        <div className='p-4'>
-            <h3 className="text-lg font-medium text-grey-900" class>CPU</h3>
+        <div className='p-4'> 
+            <h3 className="text-lg  font-medium text-grey-900" class>CPU</h3>
         </div>
 
     </div>
--- src/costumer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/costumer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,35 +1,67 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel'
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
-import { Button } from '@mui/material';
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import { Button } from "@mui/material";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 
 const HomeSectionCarousel = () => {
 
-    const responsive = {
-        0: { items: 1 },
-       720: { items: 3 },
-        1024: { items:  4},
-    };
+    const [activeIndex,setActiveInex]= useState(0);
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5 },
+  };
+  const slidePrev=()=>setActiveInex(activeIndex-1);
+  const slideNext=()=>setActiveInex(activeIndex+1);
 
-    const items= [1,1,1,1,1].map((item)=><HomeSectionCard/>)
+  const syncActiveIndex=({item})=>setActiveInex(item);
+
+  const items = [1, 1, 1,1,1,1,1,1,1,1, 1, 1].map((item) => <HomeSectionCard />);
   return (
-    <div className='relative px-4 lg:px-8'> 
-    <div className='relative p-5 '>
- <AliceCarousel
-     items={items}
-    disableButtonsControls
-   
-    autoPlayInterval={1000}
-    infinite
-    responsive={responsive} 
-    />
-    <Button  variant="contained" className='z-50' sx={{position:"absolute", top:'8rem', right:'0rem', transform:'translateX(50%) rotate(90deg) ',bgcolor:'white'}}  ria-label="next">
-    <ArrowLeftIcon sx={{transform:"rotate(90deg)"}}/>
-    </Button>
+    <div className="  ">
+      <div className="relative p-5 ">
+        <AliceCarousel
+          items={items}
+          disableButtonsControls
+          autoPlayInterval={1000}
+          infinite
+          responsive={responsive}
+          disableDotsControls
+          onSlideChange={syncActiveIndex}
+          activeIndex={activeIndex}
+        />
+        <Button
+          variant="contained"
+          className="z-50"
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            right: "0rem",
+            transform: "translateX(50%) rotate(90deg) ",
+            bgcolor: "white",
+          }}
+          ria-label="next"
+        >
+          <ArrowLeftIcon sx={{ transform: "rotate(90deg)", color: "black" }} />
+        </Button>
+        <Button
+          variant="contained"
+          className="z-50"
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            left: "0rem",
+            transform: "translateX(-50%) rotate(-90deg) ",
+            bgcolor: "white",
+          }}
+          ria-label="next"
+        >
+          <ArrowLeftIcon sx={{ transform: "rotate(90deg)", color: "black" }} />
+        </Button>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default HomeSectionCarousel
+export default HomeSectionCarousel;
