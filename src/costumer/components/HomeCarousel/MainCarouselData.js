1832c07c2cc9a8b2f7032fa0fbd37b6dd14a95a9
Nimesh <dhungelnimesh5@gmail.com>
2024-02-28 18:11:33 +0545

    pages and owl carousel etc

---
+++ src/costumer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,14 @@
+export const mainCarouselData = [
+  {
+    image: "http://surl.li/raebu",
+    path: "",
+  },
+  {
+    image: "http://surl.li/raebu",
+    path: "",
+  },
+  {
+    image: "http://surl.li/raebu",
+    path: "",
+  },
+];
